e73219a90f594f52975f0d4cb8a4fc6f2afee692
Your Nameitti2004 <6639013001@cdti.ac.th>
2023-06-21 12:39:00 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="style.css"/>
+
+<?php
+$name = "Sira Thovadee";
+$age = 17;
+$year = 2549;
+$byear = $year-$age;
+
+echo " <h1> สวัสดีมนุษย์ </h1>  ";
+echo"จัดทำโดย$name<br/>";
+echo"พ.ศ.เกิด$byear";
+echo"<img src='images/images.jpg' />";
+
+
+?>
+<p class="uuu">
+ชาวนา
+</p>
---
+++ login.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" href="style.css"/>
+
+<div id="box1">
+    login
+</div>
